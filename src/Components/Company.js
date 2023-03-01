b34d71bc01a0823154e1e591be3bccd577b50ce1
vrushalimakwana <vrushali.makwana@ghanshyamdigital.com>
2023-03-01 17:55:37 +0530

    nested routing

---
+++ src/Components/Company.js
@@ -0,0 +1,10 @@
+function Company() {
+    return (
+        <div>
+            <h1>Company Page</h1>
+            <p>This is my Company Info</p>
+        </div>
+    )
+}
+
+export default Company;
