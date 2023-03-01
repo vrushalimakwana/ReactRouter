70544756d3e40ddd8ff96d0e630adb4e16dc73fa
vrushalimakwana <vrushali.makwana@ghanshyamdigital.com>
2023-03-01 14:35:22 +0530

    navigating

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,20 @@
-function Home() {
-    return<>
-        <h1>Home Page</h1>
-    </>
-}
+import {Link, useNavigate} from "react-router-dom";
 
+function Home() {
+    const navigate = useNavigate();
+    const navToPage = (url)=> {
+        navigate(url)
+    }
+
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <Link to = "/about">Go to About Page</Link>
+            <br /> <br/>
+            <button onClick={()=> navToPage('/about')}>Go to About Page</button>
+            <br /> <br/>
+            <button onClick={()=> navToPage('/filter')}>Go to Filter Page</button>
+        </div>
+    )
+}
 export default Home;
